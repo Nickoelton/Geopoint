ff55dbde02f8585b59689dcb6ec044cd4a5085f8
Elton Rodriguez Dulanto <nick_22_04@hotmail.com>
2017-06-24 10:51:14 -0500

    Primera Subida
    El proyecto

---
+++ DemoGeoPoint/DemoGeoPoint.Android/SplashScreenActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DemoGeoPoint.Droid
+{
+    [Activity(Label = "DemoGeoPoint", Theme="@style/Theme.Splash", MainLauncher =true, NoHistory =true)]
+    public class SplashScreenActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Intent i = new Intent(this, typeof(MainActivity));
+            StartActivity(i);
+
+            // Create your application here
+        }
+    }
+}
